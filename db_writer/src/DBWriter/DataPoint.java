8283a5d4092470fda686b846bc872aa4ff064f0c
remi <remi@de180811>
2019-01-15 12:05:41 +0100

    Fix incorrect formatting of temp
    Was in full integers of tenth degrees (1.5C became 15), is now in full,
    with always one decimal (1.5C is now 1.5).

--- db_writer/src/DBWriter/DataPoint.java
+++ db_writer/src/DBWriter/DataPoint.java
@@ -1,5 +1,14 @@
 package DBWriter;
 
+import java.text.DecimalFormat;
+
+/**
+ * DataPoint is a single state of a specific weather station.
+ * It stores the weather station ID and the measured values.
+ * 
+ * @author remi
+ *
+ */
 public class DataPoint implements Comparable<DataPoint>
 {
 	public int clientID;
@@ -7,15 +16,26 @@ public class DataPoint implements Comparable<DataPoint>
 
 	private String dbLine;
 
+	DecimalFormat df = new DecimalFormat("#.#");
+
 	@Override
 	public int compareTo(DataPoint dp2) {
 		return this.clientID - dp2.clientID;
 	}
 	
+	/**
+	 * makeDBLine formats the DataPoint to a string in the format of
+	 * the db_writer database files.
+	 * 
+	 * The string is formatted as:
+	 * {clientID}={temp in always one decimal}
+	 * 
+	 * @return String Formatted string
+	 */
 	public String makeDBLine()
 	{
 		if (this.dbLine == null) {
-			this.dbLine = this.clientID + "=" + this.temp;
+			this.dbLine = String.format("%d=%.01f", this.clientID, ((float)this.temp)/10);
 		}
 		
 		return this.dbLine;
