5bbfd844e1162f6b9662a454ff81b76d995787f7
hilkojj <hilkojj@outlook.com>
2019-01-17 14:17:51 +0100

    selecting countries

--- awaagular/src/app/pages/export/export.component.ts
+++ awaagular/src/app/pages/export/export.component.ts
@@ -8,7 +8,7 @@ import { StationsService } from 'src/app/services/stations.service';
 })
 export class ExportComponent implements OnInit {
 
-    expanded: {[country: string]: boolean} = {}
+    expanded: { [country: string]: boolean } = {}
     selectedStationIds: number[] = []
 
     constructor(
@@ -22,6 +22,25 @@ export class ExportComponent implements OnInit {
         if (!option.selected)
             this.selectedStationIds = this.selectedStationIds.filter(id => id != option.value)
         else this.selectedStationIds.push(option.value)
+    }
+
+    countrySelect(country: string, checkAll: boolean) {
+        let stationIds = this.stations.byCountry[country].map(st => st.id)
+        if (!checkAll)
+            this.selectedStationIds = this.selectedStationIds.filter(id => !stationIds.includes(id))
+        else
+            stationIds
+                .filter(id => !this.selectedStationIds.includes(id))
+                .forEach(id => this.selectedStationIds.push(id))
+    }
+
+    // returns 2 if all selected, 1 if some, 0 if none
+    countrySelected(country: string) {
+        let stations = this.stations.byCountry[country]
+        let selected = stations.filter(st => this.selectedStationIds.includes(st.id))
+        if (selected.length == stations.length)
+            return 2
+        return selected.length ? 1 : 0
     }
 
 }
