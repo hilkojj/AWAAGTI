cedfac5e33f93e3a628f8ab04d8d76c2418f975f
hilkojj <hilkojj@outlook.com>
2019-01-17 12:32:17 +0100

    Create configs.service.ts

---
+++ awaagular/src/app/services/configs.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+export interface TimeFrame {
+    from: number
+    to: number
+    interval: 3600 | 60 | 86400 | 2592000 | 1
+}
+
+type measurementType = "temperature" | "windSpeed"
+
+export interface Config {
+    name: string
+    stationIds: number[]
+    timeFrame: TimeFrame
+    what: measurementType[]
+    sortBy?: measurementType
+    limit?: number
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ConfigsService {
+
+    constructor() { }
+
+}
