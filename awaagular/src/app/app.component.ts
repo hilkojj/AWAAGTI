d29deef4d247bf525e1268fe6d3e65e5ec908ace
hilkojj <hilkojj@outlook.com>
2019-01-17 10:48:58 +0100

    toolbar

--- awaagular/src/app/app.component.ts
+++ awaagular/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'awaagular';
+
+    atTop = true
+
+    constructor() {
+        setInterval(() => {
+            this.atTop = window.scrollY < 10
+        }, 100)
+    }
+
 }
