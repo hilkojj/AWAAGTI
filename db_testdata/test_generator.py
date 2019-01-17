49f1f4bf09467af6a310e8122a5e28a02ff5b638
timostrating <timostrating@home.nl>
2019-01-17 15:37:49 +0100

    better test data

--- db_testdata/test_generator.py
+++ db_testdata/test_generator.py
@@ -1,13 +1,13 @@
 import random
 
-MAX_SIZE = 60
+MAX_SIZE = 70
 
 f = open("test.txt","w+")
 
 for i in range(8000):
     s = f"{i}="
     for i in range(10):
-        s += str(float(random.randint(-999,999)) / 10)
+        s += str(float(random.randint(-999,999)) / 10) + ","
     while len(s) < MAX_SIZE:
         s += "#"
 
