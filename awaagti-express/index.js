40f444ecaee9dfcd577ce18d93a4574fb138bb5c
timostrating <timostrating@home.nl>
2019-01-18 12:04:06 +0100

    express test

--- awaagti-express/index.js
+++ awaagti-express/index.js
@@ -3,4 +3,23 @@ const express = require("express")
 const app = express()
 const port = 8080
 
+
+
+testjson = {
+    name: "DIT IS MIJN MOOIE EXPORT",
+    stationIds: [50, 100, 7900, 7950],
+    timeFrame: {"from": 12123123123, "to": 123123123, "interval": 3600},
+    what: "temperature",
+    sortBy: "temperature",
+    limit: "100"
+}
+
+var net = require('net');
+
+var client = new net.Socket();
+client.connect(12345, '127.0.0.1', function() {
+	console.log('Connected');
+	client.write(JSON.stringify(testjson));
+});
+
 app.listen(port, () => console.log("AWAAGTI-express svr running on port " + port))
