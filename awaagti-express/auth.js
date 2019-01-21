48b7587e82d04190344c9f670202e90ecd31342e
hilkojj <hilkojj@outlook.com>
2019-01-21 14:02:21 +0100

    very much user login/register system with JWT

--- awaagti-express/auth.js
+++ awaagti-express/auth.js
@@ -4,22 +4,74 @@ const jwt = require('jsonwebtoken')
 const fs = require("fs")
 const secret = fs.readFileSync("./secret.txt").toString()
 
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
 const usersFile = "./users.json"
 
 // load users from .json file.
 const users = fs.existsSync(usersFile) ? JSON.parse(fs.readFileSync(usersFile).toString()) : {}
 
-passport.use(new LocalStrategy((username, password, done) => {
-    
-    let user = users[username]
-    if (!user)
-        done("Username does not exist")
+const saveUser = async (username, password) => {
+    users[username] = {
+        username: username,
+        password: await bcrypt.hash(password, 3),
+        registeredTimestamp: Date.now()
+    }
+    console.log("A very new user has regsitered:", username)
+    saveUsers()
+}
 
-    await bcrypt.compare(password, user.password) ? done()
+const saveUsers = () =>
+    fs.writeFile(usersFile, JSON.stringify(users), console.error)
+
+passport.use(new LocalStrategy({
+    usernameField: "username",
+    passwordField: "password"
+}, async (username, password, done) => {
+
+    let user = users[username]
+
+    if (!user)
+        return done(null, null, { message: "Username does not exist" })
+
+    await bcrypt.compare(password, user.password)
+        ?
+        done(null, user)    // password is correct
+        :
+        done(null, null, { message: "Passport is wrong" })
 
 }))
 
-module.exports.generateJWT = username => jwt.sign({ username }, secret)
+const generateJWT = username => jwt.sign(username, secret)
+
+module.exports.login = (req, res) => {
+    passport.authenticate("local", (err, user, info) => {
+        if (err)
+            return res.status(500).json(err)
+
+        if (user)
+            return res.status(200).json({
+                token: generateJWT(user.username)
+            })
+        else return res.status(401).json(info)
+
+    })(req, res)
+}
+
+module.exports.register = async (req, res) => {
+    let username = String(req.body.username || "")
+    let password = String(req.body.password || "")
+
+    if (!username.length)
+        return res.status(400).json({ message: "A username is required" })
+    if (username.match(/W+/))
+        return res.status(400).json({ message: "A username can only contain normal characters and numbers" })
+    if (!password.length)
+        return res.status(400).json({ message: "A password is required" })
+    if (users[username])
+        return res.status(400).json({ message: "Username already exists" })
+
+    await saveUser(username, password)
+    module.exports.login(req, res)
+}
--- awaagti-express/index.js
+++ awaagti-express/index.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
 const port = 8080
 
+app.use(bodyParser.json())
+
 const auth = require("./auth.js")
+const passport = require('passport')
+
+app.use(passport.initialize())
+app.post("/api/login", auth.login)
+app.post("/api/register", auth.register)
 
 app.listen(port, () => console.log("AWAAGTI-express svr running on port " + port))
