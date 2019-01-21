61a1b27a2117c73f4a0495586579062e4cff245f
hilkojj <hilkojj@outlook.com>
2019-01-21 15:22:23 +0100

    better Access-Control-Allow-Origin and -Headers

--- awaagti-express/index.js
+++ awaagti-express/index.js
@@ -5,6 +5,11 @@ const app = express()
 const port = 8080
 
 app.use(bodyParser.json())
+app.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization")
+    next()
+});
 
 const auth = require("./auth.js")
 const passport = require('passport')
