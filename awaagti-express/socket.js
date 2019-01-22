af4966cfb1f289523b4791383bcbc3459bc28d20
hilkojj <hilkojj@outlook.com>
2019-01-22 14:25:00 +0100

    very nice error message

--- awaagti-express/socket.js
+++ awaagti-express/socket.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken')
 const auth = require("./auth")
+const exportConfig = require("./export")
 
 module.exports = io => {
-    
+
     io.on('connection', clientSocket => {
         console.log("hi someone connected")
         clientSocket.on('jwt', token => {
@@ -21,6 +22,26 @@ const initClient = (socket, user) => {
     console.log("hi", user.username)
 
     socket.on("export", config => {
-        
+
+        config.name = String(config.name || "")
+
+        if (!config.name.length)
+            return
+
+        exportConfig(
+            config,
+            progress => {
+                console.log("Export progress " + config.name, progress)
+                socket.emit("export progress " + config.name, progress)
+            },
+            file => {
+                console.log("Export can be found in", file)
+                socket.emit("export done" + config.name, file)
+            },
+            err => {
+                console.log("Export error " + config.name)
+                socket.emit("export error " + config.name, err)
+            }
+        )
     })
 }
