1366712f86802d7029fcab95408902cfc6e63dc8
hilkojj <hilkojj@outlook.com>
2019-01-23 16:03:28 +0100

    added map with stations

--- awaagular/src/app/pages/export/export.component.ts
+++ awaagular/src/app/pages/export/export.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { StationsService, Station } from 'src/app/services/stations.service';
 import { Config, ConfigsService, measurementType } from 'src/app/services/configs.service';
+
+declare var L: any
 
 @Component({
     selector: 'app-export',
     templateUrl: './export.component.html',
     styleUrls: ['./export.component.scss']
 })
-export class ExportComponent implements OnInit {
+export class ExportComponent implements OnInit, DoCheck {
 
     expanded: { [country: string]: boolean } = {}
 
     searchInput = ""
 
     config: Config
+
+    map
 
     measurementSelect(selected: boolean, name: measurementType) {
         if (selected && !this.config.what.includes(name))
@@ -47,6 +51,41 @@ export class ExportComponent implements OnInit {
             d.setSeconds(0)
         })
         this.dateChanged()
+
+        this.map = new L.Map("map").setView([40, 70], 2)
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=sk.eyJ1IjoibG9zb3MiLCJhIjoiY2pueXhwZ3RvMDFzdzNrbXFicnlmY3Q1YiJ9.ixxDbuWyXhWX4PY4qt07ZA', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'your.mapbox.access.token'
+        }).addTo(this.map)
+    }
+
+    markersAdded = false
+
+    ngDoCheck() {
+        if (this.markersAdded || this.stations.array.length == 0)
+            return
+
+        let icon = new L.DivIcon({
+            className: 'station-icon',
+            html: 'bla'
+        })
+
+        var markerClusters = L.markerClusterGroup({
+            maxClusterRadius: 80
+        })
+
+        this.stations.array.forEach(st => {
+            try {
+                markerClusters.addLayer(
+                    L.marker([st.lat, st.lon], { icon: icon })
+                )
+            } catch (e) { }
+        })
+        this.map.addLayer(markerClusters)
+        this.markersAdded = true
     }
 
     selectionChange(option) {
