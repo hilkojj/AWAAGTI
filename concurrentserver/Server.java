1724f67060f6a42c2f045087f2f8daebef7c76da
Chris Roscher <chri@aitai.nl>
2019-01-24 14:27:49 +0100

    Store temps in ring array and calculate average

---
+++ concurrentserver/Server.java
@@ -0,0 +1,39 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+
+public class Server {
+	public static final int PORT = 7789;
+	private static final int maxnrofConnections=800;
+
+    private static int head = 0;
+    private static int tail = 0;
+
+	// public static Map<Integer, Float> data = new ConcurrentHashMap<Integer, Float>();
+	public static int []revStation = new int[8010];
+	public static String[][] data = new String[8000][60];
+	public static Map<Integer, Integer> stations = new ConcurrentHashMap<Integer, Integer>();
+	public static FixedRingArray[] queues = new FixedRingArray[8010];
+	public static void main(String[] args) {
+			for (int i = 0; i < 8010; i++) {
+			queues[i] = new FixedRingArray();
+		}
+		Socket connection;
+		try {
+			ServerSocket server = new ServerSocket(PORT);
+			System.err.println("Server started with a maximum of: " + maxnrofConnections + " Connections");
+
+			while (true) {
+				connection = server.accept();
+				System.err.println("New connection accepted. handing it over to worker thread");
+				Thread worker = new Thread(new WorkerThread(connection));
+				worker.start();
+			}
+		}
+
+		catch (java.io.IOException ioe) {
+			System.err.print("\n\nIOException\n\n");
+		}
+	}
+}
---
+++ concurrentserver/WorkerThread.java
@@ -0,0 +1,213 @@
+import java.net.*;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.*;
+
+class WorkerThread implements Runnable  {
+	private Socket connection;
+
+	private final static boolean SHOW_DEBUG = true;
+	private final static boolean SHOW_ERROR = true;
+	private final static boolean DATABASE = false;
+	private static int num = 0;
+
+    public boolean debug = false;
+
+	public WorkerThread(Socket connection) {
+		this.connection = connection;
+	}
+
+	public void run() {
+		print("Worker thread started\n");
+
+		Connection conn = null;
+		String query = "insert into measurements (stn, date, time, temp, dewp, stp, slp, visib, wdsp, prcp, sndp, frshtt, cldc, wnddir) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		final int batchSize = 10; //Batch size is important
+		int insertCount=0;
+
+		try {
+
+			// if (DATABASE) Class.forName("com.mysql.jdbc.Driver").newInstance();
+			// if (DATABASE) conn = DriverManager.getConnection("jdbc:mysql://localhost/unwdmi?user=root&password=lol123");
+
+
+			String s;
+
+			BufferedReader bin = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			String[] input = new String[14];
+			boolean fill = false;
+
+			Map<String, Integer> lookup = new HashMap<String, Integer>();
+			lookup.put("STN", 0);
+			lookup.put("DATE", 1);
+			lookup.put("TIME", 2);
+			lookup.put("TEMP", 3);
+			lookup.put("DEWP", 4);
+			lookup.put("STP", 5);
+			lookup.put("SLP", 6);
+			lookup.put("VISIB", 7);
+			lookup.put("WDSP", 8);
+			lookup.put("PRCP", 9);
+			lookup.put("SNDP", 10);
+			lookup.put("FRSHTT", 11);
+			lookup.put("CLDC", 12);
+			lookup.put("WNDDIR", 13);
+
+
+
+			while ((s = bin.readLine()) != null) {
+//
+				if (s.equals("\t</MEASUREMENT>")) {
+
+					// if (DATABASE) {
+					// 	PreparedStatement preparedStmt = conn.prepareStatement(query);
+					// 	for (int i=1; i <= input.length; i++)
+					// 		preparedStmt.setString (i, input[i-1]);
+                    //
+					// 	preparedStmt.addBatch();
+					// 	if (++insertCount % batchSize == 0) {
+					// 		preparedStmt.executeBatch();
+					// 	}
+					// }
+                    //
+					mapIncrement(Integer.parseInt(input[0]));
+					// print(Server.stations.size());
+					if(Server.stations.get(Integer.parseInt(input[0])) != null) {
+						if(!fill)
+							Server.queues[Server.stations.get(Integer.parseInt(input[0]))].put(Float.parseFloat(input[3]));
+						if(Server.queues[Server.stations.get(Integer.parseInt(input[0]))].hasBeanRound || fill) {
+							fill = true;
+							System.out.printf("Station: %d (%s), Temp: %s\n", Server.stations.get(Integer.parseInt(input[0])), input[0], Arrays.asList(Server.queues[Server.stations.get(Integer.parseInt(input[0]))]));
+							float sum = 0;
+							for (int i = 0; i < 10; i++) {
+								sum += Server.queues[Server.stations.get(Integer.parseInt(input[0]))].get(i);
+							}
+							sum /=10;
+							System.out.printf("Avg last 5: %f\nrevStation: %d\n", sum, Server.revStation[Server.stations.get(Integer.parseInt(input[0]))]);
+						}
+						// Server.data.putIfAbsent(Integer.parseInt(input[0]), Float.parseFloat(input[3]));
+						// try{putData(Server.stations.get(Integer.parseInt(input[0])), Float.parseFloat(input[3]));}
+						// catch(Exception e){print("error");}
+					}
+					// if(Server.data[Server.stations.get(Integer.parseInt(input[0]))][10] != null) {
+					// 		System.out.printf("Data: %s", Arrays.asList(Server.data[Integer.parseInt(input[0])]));
+					// 		Arrays.fill(Server.data[Integer.parseInt(input[0])], null);
+					// }
+					// for (int i = 0; i < 8000; i++) {
+					// 	if (Server.data[i] != null) {
+					// 		// send(Server.data);
+					// 		System.out.printf("Data: %s", Arrays.asList(Server.data[i]));
+					// 		// Server.data.clear();
+					// 		Arrays.fill(Server.Data[i], null);
+					// 	}
+					// }
+					continue;
+				}
+
+				int start = 0; // TODO: we currently only parse xml that opens and closes a tag on the same line
+				int end = 0;
+				int lookup_index = 0;
+				String value = "";
+
+				int i = 0;
+				for (;i < s.length(); i++) {
+					if(s.charAt(i) == '>') {
+						start = i;
+					}
+					else if(start != 0 && s.charAt(i) == '/') {
+						value = s.substring(start +1, i-1);
+						end = i;
+						break;
+					}
+				}
+
+				if(start != 0 && end != 0) {
+					for (; i < s.length(); i++) {
+						if (s.charAt(i) == '>') {
+							try {
+								lookup_index = lookup.get(s.substring(end + 1, i));
+							} catch (NullPointerException e) { error("UNSUPPORTED TAG in XML INPUT: " + s.substring(end + 1, i)); break; }
+
+							if (end - start <= 2) {
+								// print("EMPTY DATA FOR " + s.substring(end + 1, i));
+							} else {
+								input[lookup_index] = value;
+							}
+							break;
+						}
+					}
+				// int sum = 0;
+				// for (int d : input[3]) sum += d;
+				}
+	        }
+
+			// now close the socket connection
+			conn.close();
+			connection.close();
+			error("Connection closing");
+		}
+		catch (Exception e) {
+			print("Connection closed unexpectedly");
+			// System.err.println("Exception: " + Arrays.toString(e.getStackTrace()) + e.getMessage());
+		}
+	}
+
+	private static void print(Object o) {
+		if (SHOW_DEBUG)
+			System.out.println(o);
+	}
+
+	private static void error(Object o) {
+		if (SHOW_ERROR)
+			System.err.println(o);
+	}
+
+	private static synchronized void putData(Integer station, float temp) {
+		String[] dataArray = Server.data[station];
+		for (int i = 0; i < dataArray.length; i++) {
+			if (dataArray[i] != null) {
+				dataArray[i] = Float.toString(temp);
+				break;
+			}
+		}
+	}
+	private static synchronized void mapIncrement(Integer station) {
+		if (Server.stations.get(station) == null) {
+		// Server.stations.putIfAbsent(station, num);
+			Server.stations.put(station, num);
+			num++;
+			// Add the stationID to a ArrayList.
+			// So we can do arraylist.get(index) to get the stationID.
+			Server.revStation[num] = station;
+		}
+	}
+}
+
+/*
+
+<!--Het weatherdata-element bevat meerdere measurement-elementen-->
+    <WEATHERDATA>
+
+    10 x  <MEASUREMENT>
+        <STN>123456</STN>
+        <DATE>2009-09-13</DATE>
+        <TIME>15:59:46</TIME>
+        <TEMP>-60.1</TEMP>
+        <DEWP>-58.1</DEWP>
+        <STP>1034.5</STP>
+        <SLP>1007.6</SLP>
+        <VISIB>123.7</VISIB>
+        <WDSP>10.8</WDSP>
+        <PRCP>11.28</PRCP>
+        <SNDP>11.1</SNDP>
+        <FRSHTT>010101</FRSHTT>
+        <CLDC>87.4</CLDC>
+        <WNDDIR>342</WNDDIR>
+    </MEASUREMENT>
+
+    </WEATHERDATA>
+
+*/
