4e3bbb7f1cbbb049f46fd05133b6c66e45573741
hilkojj <hilkojj@outlook.com>
2019-01-24 12:24:17 +0100

    exports done

--- awaagti-express/export.js
+++ awaagti-express/export.js
@@ -1,4 +1,5 @@
 const net = require('net')
+const fs = require('fs')
 
 //stations=1234,1356;from=23423423;to=3453454353;interval=1;what=temp,sfgfdgd;sortBy=32432432;limit=10;filter=temp,<,10\n
 
@@ -16,6 +17,17 @@ const configToQuery = config => {
 module.exports = (config, onProgress, onDone, onError) => {
 
     let client = new net.Socket()
+    let file = null
+
+    let interval = setInterval(() => {
+        if (!file) return
+        fs.exists(file, exists => {
+            if (!exists) return
+            onDone(file)
+            clearInterval(interval)
+        })
+    }, 1000)
+
     client.connect(12345, '127.0.0.1', () => {
         console.log('Connected')
         client.write(configToQuery(config))
@@ -24,5 +36,25 @@ module.exports = (config, onProgress, onDone, onError) => {
         console.error(err)
         onError("Timos server is not running. Blame timo")
     })
+    client.on("data", data => {
+        console.log(data)
+        if (data.startsWith("file="))
+            file = data.split("file=")[1]
 
+        if (data.startsWith("error="))
+            onError(data.split("error=")[1])
+
+        if (data.startsWith("progress="))
+            onProgress(Number(data.split("progress=")[1]))
+    })
+    client.on("end", () => {
+        client.end()
+    })
+
+    return {
+        end: () => {
+            client.end()
+            clearInterval(interval)
+        }
+    }
 }
