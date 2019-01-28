579f1fda9d98638e7486e9499904ca901d984aa3
timostrating <timostrating@home.nl>
2019-01-28 18:15:47 +0100

    Hilko magic

--- db/src/DBReader/Query.java
+++ db/src/DBReader/Query.java
@@ -21,14 +21,15 @@ public class Query {
 
     public int hash = -1;
 
-    public int[] stations = {};
-    public long from = 0;
-    public long to = 0;
-    public int interval = 1;
-    public ArrayList<String> what = new ArrayList<>();
-    public String sortBy = "temperature";
-    public int limit = 10;
-    public QueryFilter filter;
+    private int[] stations = {};
+    private long from = 1500000000;
+    private long to = -1;
+    private int interval = 1;
+    private ArrayList<String> what = new ArrayList<>();
+    private String sortBy = "temperature";
+    private boolean distinct = false;
+    private int limit = -1;
+    private QueryFilter filter;
 
     public Query(String options) throws Exception {
         try {
@@ -46,6 +47,7 @@ public class Query {
                     case "interval": interval = Integer.parseInt(data); break;
                     case "what":  what.addAll(Arrays.asList(data.split(","))); break;
 //                    case "sortBy": sortBy = data; break;
+//                    case "distinct": distinct = false; break;
                     case "limit": limit = Integer.parseInt(data); break;
                     case "filter": this.filter = new QueryFilter(data); break;
                     default:
@@ -57,21 +59,60 @@ public class Query {
             Logger.error(e.getMessage());
             throw new Exception("Your query does not have the proper syntax");
         }
+
+        if (to == -1)
+            to = System.currentTimeMillis() / 1000;
+
+        if (limit == -1)
+            limit = Integer.MAX_VALUE;
     }
 
     // TEST
     public static void main(String[] args) {
         try {
-            Query q1 = new Query("stations=1234,1356;\n");
-            AtomicInteger length = new AtomicInteger();
-            q1.getDataFiles().forEach(x -> length.addAndGet(1));
-            System.out.println("1 " + (length.get() == 4));
+            new Query("limit=10;stations=1234,1356;from=23423423;sortBy=32432432;to=3453454353;interval=1;\n");
+            System.out.println("Syntax: 1");
 
             new Query("stations=1234,1356;from=23423423;to=3453454353;interval=1;sortBy=32432432;limit=10;filter=temp,>,-1;\n");
-            System.out.println("2");
+            System.out.println("Syntax: 2");
 
             new Query("stations=1234,1356;from=23423423;to=3453454353;interval=1;what=temp,sfgfdgd;sortBy=32432432;limit=10;filter=temp,<,10\n");
-            System.out.println("3");
+            System.out.println("Syntax: 3");
+
+
+
+            Query q1 = new Query("stations=1234,1356;\n");
+            int i = 0;
+            Iterator<File> iterator1 = q1.getDataFilesNormal().iterator();
+            while(iterator1.hasNext()) {
+                i++;
+            }
+
+            System.out.println("PARSE: 1 " + (i == 5) + " _ " + i);
+
+
+            Query q2 = new Query("stations=50,7950;from=0;to=-1\n");
+            int i2 = 0;
+            Iterator<File> iterator2 = q2.getDataFilesNormal().iterator();
+            while(iterator2.hasNext()) {
+                i2++;
+                System.out.println(iterator2.next());
+            }
+
+            System.out.println("PARSE: 2 " + (i2 == 5) + " _ " + i2 );
+
+
+            Query q3 = new Query("stations=50,7950;from=1548348440;to=1548348442\n");
+            int i3 = 0;
+            Iterator<File> iterator3 = q3.getDataFilesNormal().iterator();
+            while(iterator3.hasNext()) {
+                i3++;
+                System.out.println(iterator3.next());
+            }
+
+            System.out.println("PARSE: 3 " + (i3 == 3) + " _ " + i3 );
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -85,38 +126,115 @@ public class Query {
 
 
     public Iterable<File> getDataFiles() {
-        return () -> {
-            try {
-                return new Iterator<File>() {
-                    long start = from;
-                    long iteration = 0;
-                    File next_val = null;
+        try {
+            if (sortBy == null)
+                return getDataFilesNormal();
+            else
+                return getDataFilesSorted();
 
-                    @Override
-                    public boolean hasNext() {
-                        if (iteration >= limit)
-                            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return (Iterable<File>) Collections.emptyIterator();
+        }
+    }
 
-                        while (start < to && next_val != null) {
-                            next_val = new File(Settings.DATA_PATH + "/" + start);
-                            start += interval;
+    /*
+        Answer Queries iteratively over all the data
+     */
+    private Iterable<File> getDataFilesNormal() {
+        return () -> new Iterator<File>() {
+            long cur = from;
+            long iteration = 0;
+            File nextVal = null;
+            String currentPath = "";
+
+            @Override
+            public boolean hasNext() {
+                if (iteration >= limit)
+                    return false;
+
+                while (cur <= to) {
+
+
+//                    System.out.println("WOWIE I AM GOING TO LOOK FOR A DIR");
+
+                    if (!findDir()) return false;
+
+//                    System.out.println("WOWIE I HAVE FOUND A DIR");
+
+                    String filename = Settings.DATA_PATH + "/" + currentPath.replaceAll("(.{2})", "$1/") + "/" + cur + ".txt";
+//                    System.out.println(filename);
+                    nextVal = new File(filename);
+                    cur += interval;
+                    if (nextVal.exists()) return true;
+                }
+
+                return false;
+            }
+
+            @Override
+            public File next() {
+                if(nextVal == null)
+                    throw new NoSuchElementException();
+
+                iteration ++;
+                return nextVal;
+            }
+
+            private boolean findDir() {
+                String timestampStr = "" + cur;
+                if (currentPath.length() == 8 && timestampStr.startsWith(currentPath)) return true;
+                goDeeper:
+                while (currentPath.length() / 2 < 4 || !timestampStr.startsWith(currentPath)) {
+
+                    if (timestampStr.startsWith(currentPath)) {
+
+                        String currentPathWithSlashes = Settings.DATA_PATH + "/" + currentPath.replaceAll("(.{2})", "$1/");
+//                        System.out.println(currentPathWithSlashes);
+
+                        String[] directories = new File(currentPathWithSlashes).list();
+
+                        for (String dir : directories) {
+                            String path = currentPath + dir;
+                            if (timestampStr.startsWith(path)) {
+                                currentPath = path;
+
+                                if (currentPath.length() == 8) return true; // dir was found, cannot go any deeper ;-(
+
+                                continue goDeeper; // dir was found, eg: 15/48 was found, now find 15/48/34
+                            }
                         }
+                        // no dir found
 
-                        return start < to;
-                    }
+                        int antiDeepness = 4 - currentPath.length() / 2;
+                        long minimalTimestamp = cur + (int) Math.pow(100, antiDeepness);
+                        while (cur < minimalTimestamp) cur += interval;
 
-                    @Override
-                    public File next() {
-                        if(next_val == null || iteration >= limit)
-                            throw new NoSuchElementException();
+                        return findDir();
+                    } else currentPath = currentPath.substring(0, currentPath.length() - 2);
 
-                        iteration ++;
-                        return next_val;
-                    }
-                };
-            } catch (Exception e) {
-                e.printStackTrace();
-                return Collections.emptyIterator();
+                }
+                return false;
+            }
+        };
+    }
+
+    /*
+        Answer to Queries with index results
+     */
+    private Iterable<File> getDataFilesSorted() {
+        return () -> new Iterator<File>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public File next() {
+                if(!hasNext())
+                    throw new NoSuchElementException();
+
+                return null;
             }
         };
     }
