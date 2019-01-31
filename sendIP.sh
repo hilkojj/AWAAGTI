a8d1a1958aa9a670232db3e82081d40cab5ade0d
Chris Roscher <chris@aitai.nl>
2019-01-31 16:58:37 +0100

    Read api key from file

--- sendIP.sh
+++ sendIP.sh
@@ -1,5 +1,6 @@
 ip=`hostname -I|xargs`
+api=$(</home/pi/apiKey)
 for (( i = 0; i < 12; i++ )); do
-	curl -X POST -H "Content-Type: application/json" --data "{\"apiKey\": \"hallo ik ben chris en ik hou niet van windows en proprietary shit\", \"ip\": \"$ip\"}" https://university-of-tehran.ml/api/register-repsbarrie-ip
+	curl -X POST -H "Content-Type: application/json" --data "{\"apiKey\": \"$api\", \"ip\": \"$ip\"}" https://university-of-tehran.ml/api/register-repsbarrie-ip
 	sleep 5
 done
