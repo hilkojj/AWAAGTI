b6f7957f1d2b426c15cb06542a4417ab64071e3d
Chris Roscher <chris@aitai.nl>
2019-01-31 16:49:05 +0100

    Send ip every 5 seconds

--- sendIP.sh
+++ sendIP.sh
@@ -1,2 +1,5 @@
 ip=`hostname -I|xargs`
-curl -X POST -H "Content-Type: application/json" --data "{\"apiKey\": \"hallo ik ben chris en ik hou niet van windows en proprietary shit\", \"ip\": \"$ip\"}" https://university-of-tehran.ml/api/register-repsbarrie-ip
+for (( i = 0; i < 12; i++ )); do
+	curl -X POST -H "Content-Type: application/json" --data "{\"apiKey\": \"hallo ik ben chris en ik hou niet van windows en proprietary shit\", \"ip\": \"$ip\"}" https://university-of-tehran.ml/api/register-repsbarrie-ip
+	sleep 5
+done
