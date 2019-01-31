366bff6e86a36d3b9c4fd479ede6e266884c5e28
hilkojj <hilkojj@outlook.com>
2019-01-31 14:54:46 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- awaagti-express/github-webhook.js
+++ awaagti-express/github-webhook.js
@@ -8,25 +8,25 @@ module.exports = (req, res) => {
 
     console.log("o wowie someone pushed to master o no")
 
-    exec("git fetch --all; git reset --hard origin/master", { cwd: path.resolve("/../") }, (err, stdout, stderr) => {
+    exec("git fetch --all; git reset --hard origin/master", { cwd: path.resolve("../") }, (err, stdout, stderr) => {
 
         console.log(err, stderr, stdout)
         console.log("wowie i have pulled master, now lets install 5 million npm modules")
 
-        exec("npm install", { cwd: path.resolve("/../awaagular/") }, () => {
+        exec("npm install", { cwd: path.resolve("../awaagular/") }, () => {
 
             console.log(err, stderr, stdout)
             console.log("npm modules for angular installed")
 
-            exec("npm install", { cwd: path.resolve("/") }, () => {
+            exec("npm install", { cwd: path.resolve("./") }, () => {
 
                 console.log(err, stderr, stdout)
                 console.log("npm modules for node installed")
 
-                console.warn("lets use 100% CPU (lets build the angular app)")
+                console.log("lets use 100% CPU (lets build the angular app)")
 
                 exec("ng build --prod --aot", {
-                    cwd: path.resolve("/../awaagular/")
+                    cwd: path.resolve("../awaagular/")
                 }, () => {
                     console.log(err, stderr, stdout)
 
