6fa49c27b1234ed32f0fe26d0ca47670b07d3db1
hilkojj <hilkojj@outlook.com>
2019-02-01 13:08:16 +0100

    allow range header

--- awaagti-express/index.js
+++ awaagti-express/index.js
@@ -25,7 +25,7 @@ require("./socket.js")(io)
 app.use(bodyParser.json())
 app.use((_req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:4200")
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Range")
     next()
 })
 
