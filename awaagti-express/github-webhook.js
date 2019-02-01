383a905b8954054f3ec09ee199b76c5fbd791a4d
hilkojj <hilkojj@outlook.com>
2019-02-01 14:51:19 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa

--- awaagti-express/github-webhook.js
+++ awaagti-express/github-webhook.js
@@ -1,6 +1,5 @@
 const exec = require("child_process").exec
 const path = require("path")
-const fs = require("fs")
 
 // this very nice code pulls shit from master and rebuilds shit when someone pushes shit to master
 // also <3 callbacks
@@ -24,30 +23,10 @@ module.exports = (req, res) => {
                 console.log(err, stderr, stdout)
                 console.log("npm modules for node installed")
                 console.log("node server will restart before ng build is done")
-
-                exec("du -s -B1 awaagular/", {
-                    cwd: path.resolve("../")
-                }, (err, stdout, stderr) => {
-                    let size = Number(stdout.replace(/\D/g,''))
-
-                    let lastSize = 0
-                    try {
-                        lastSize = Number(fs.readFileSync("./ng-prev-size.txt").toString())
-                    } catch (e) {
-                        console.error(e)
-                    }
-
-                    if (lastSize != size) {
-
-                        console.log("Angular app source code changed\nlets use 100% CPU (lets build the angular app)")
-
-                        fs.writeFileSync("./ng-prev-size.txt", size)
-                        exec("sudo ng build --prod --aot &", {
-                            cwd: path.resolve("../awaagular/")
-                        })
-                    }
-                    exec("sudo pm2 restart 0")
+                exec("sudo ng build --prod --aot &", {
+                    cwd: path.resolve("../awaagular/")
                 })
+                exec("sudo pm2 restart 0")
             })
 
 
