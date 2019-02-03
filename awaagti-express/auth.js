e2adb4e0717df558beecaa3e06c1cb6f480c8227
hilkojj <hilkojj@outlook.com>
2019-02-03 21:29:00 +0100

    AWAAGTI EXPRESS documentation/comments

--- awaagti-express/auth.js
+++ awaagti-express/auth.js
@@ -1,7 +1,15 @@
+/**
+ * This file is responsible for User Management and Authentication using JSON Web Tokens.
+ */
 
 const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken')
 const fs = require("fs")
+
+/**
+ * The "secret" is a string that is used to encrypt the Json Web Tokens
+ * It is stored in the file "./secret.txt" and it should stay out of Git.
+ */
 const secret = fs.readFileSync("./secret.txt").toString()
 const expressJwt = require('express-jwt');
 
@@ -22,6 +30,11 @@ const users = fs.existsSync(usersFile) ? JSON.parse(fs.readFileSync(usersFile).t
 
 module.exports.users = users
 
+/**
+ * Registers a new user.
+ * @param {string} username 
+ * @param {string} password 
+ */
 const saveUser = async (username, password) => {
     users[username.toLowerCase()] = {
         username: username,
@@ -32,11 +45,17 @@ const saveUser = async (username, password) => {
     saveUsers()
 }
 
+/**
+ * Saves the users to a JSON file so that it can be loaded again whenever the server crashes or restarts.
+ */
 const saveUsers = () =>
     fs.writeFile(usersFile, JSON.stringify(users), e => e && console.error("Error writing users", e))
 
 module.exports.saveUsers = saveUsers
 
+/**
+ * Login using Passport.js authentication middleware.
+ */
 passport.use(new LocalStrategy({
     usernameField: "username",
     passwordField: "password"
@@ -55,6 +74,10 @@ passport.use(new LocalStrategy({
 
 }))
 
+/**
+ * Generates a JSON webtoken (using the secret) that can be sent to the user.
+ * @param {string} username 
+ */
 const generateJWT = username => jwt.sign({ username: username.toLowerCase() }, secret)
 
 module.exports.login = (req, res) => {
@@ -88,6 +111,9 @@ module.exports.register = async (req, res) => {
     module.exports.login(req, res)
 }
 
+/**
+ * Sends user info
+ */
 module.exports.me = async (req, res) => {
     let user = users[req.payload.username]
     res.status(user ? 200 : 401).json(user)
