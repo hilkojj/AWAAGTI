749025edd9a9d74e8fb0863afadf0153efb49c27
timostrating <timostrating@home.nl>
2019-02-04 13:27:08 +0100

    small time fix

--- db/src/DBReader/WorkerThread.java
+++ db/src/DBReader/WorkerThread.java
@@ -136,9 +136,13 @@ public class WorkerThread implements Runnable
     {
         ArrayList<DataPoint> stations = query.getStations(file);
         if (stations.size() > 0) {
-            String timestamp = file.getName().replace("sortedQuery_cache_", "" ).split("\\.")[0];
+            String timestamp = file.getName().split("\\.")[0];
 
-            xmlWriter.write("\t<datepoint time=\""+timestamp+"\" >\n"); // TODO: date=”???” time=”???”
+            if (timestamp.contains("sortedQuery_cache_"))
+                xmlWriter.write("\t<datepoint>\n");
+            else
+                xmlWriter.write("\t<datepoint time=\""+timestamp+"\" >\n"); // TODO: date=”???” time=”???”
+
             xmlWriter.write("\t\t<stations>\n");
 
             for (DataPoint station : stations) {
